76a46f55c5961021c2a5c51bf9bcd11ec58b51cd
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-06-05 15:28:37 +0800

    Export LinearGradient as named export in index.js
    A named export of LinearGradient is present in the typescript definition file (index.d.ts) but absent from the index.js implementation file
    This commit exports LinearGradient from index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 import LinearGradientIos from "./index.ios.js";
 import LinearGradientAndroid from "./index.android.js";
 
-const LinearGradient = Platform.OS === "ios"
+export const LinearGradient = Platform.OS === "ios"
   ? LinearGradientIos
   : LinearGradientAndroid;
 
